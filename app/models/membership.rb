e1e90cd522a6608536303ae79be52e7b101b396b
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-12 00:51:55 -0800

    commit tree a982fd57fffb3eddb41ba369fe3b5d404e6fe405

---
+++ app/models/membership.rb
@@ -0,0 +1,9 @@
+class Membership < ApplicationRecord
+
+  belongs_to :gym
+  belongs_to :client 
+
+  validates :gym_id, :client_id, :charge, presence: true
+  validates :client, uniqueness: true
+  
+end
